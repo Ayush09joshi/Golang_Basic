7d356f62c5792bf78a286c018cd9cfc8c92075a9
Ayush Joshi <105715149+Ayush09joshi@users.noreply.github.com>
2023-12-03 17:27:51 +0530

    Add files via upload

--- 09maps/main.go
+++ 09maps/main.go
@@ -1,8 +1,33 @@
 package main
 
-func main()  {
-	
+import "fmt"
+
+func main() {
+
 	println("Welome to Maps in Goloang!")
 
-	
+	//Creating Map.
+	languages := make(map[string]string)
+
+	//Adding elements to Map.
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+	fmt.Println("Lists of all the languages are: ", languages)
+
+
+	//Printing the value for individual key.
+	fmt.Println("JS is the key for: ", languages["JS"])
+
+
+	//Deleting the value in map.
+	delete(languages, "RB") //This method can be used in Slices as well.
+	fmt.Println("Lists of all the languages are: ", languages)
+
+
+	//Basic idea of Loops in Maps.
+	for key, value := range languages {
+		fmt.Printf("For key %v value is %v.\n" , key, value)
+	}
+
 }
---
+++ 11ifElse/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to if_else ladder in golang")
+
+	loginCout := 69
+	var result string
+
+
+	if loginCout < 10 {
+		result = "Not a Regular User"
+	} else if loginCout > 10{
+		result = "Regular User"
+	} else {
+		result = "Common User"
+	}
+	fmt.Println(result)
+
+
+	//Initalising the value on the go in if_else ladder
+	if num := 3; num <10 {
+		fmt.Println("Number is less than 10")
+	} else{
+		fmt.Println("Number is more than 10")
+	} 
+
+}
